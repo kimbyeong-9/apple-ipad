edc42af705cac8fa2048caaae77753b2bed1ba70
byeongho Kim <gkrjawnrkf1@gmail.com>
2025-05-28 21:12:10 +0900

    반응형 프젝 구현

--- js/main.js
+++ js/main.js
@@ -45,7 +45,10 @@ const searchInputEl = searchWrapEl.querySelector('input');
 const searchDelayEls = [...searchWrapEl.querySelectorAll('li')];
 
 searchStarterEl.addEventListener('click',showSearch);
-searchCloserEl.addEventListener('click',hideSearch);
+searchCloserEl.addEventListener('click', function (event) {
+    event.stopPropagation();
+    hideSearch();
+});
 searchShadowEl.addEventListener('click',hideSearch);
 
 
@@ -55,7 +58,7 @@ searchShadowEl.addEventListener('click',hideSearch);
 
 function showSearch() {
     headerEl.classList.add('searching');
-    document.documentElement.classList.add('fixed');
+    stopScroll()
     headerMenuEls.reverse().forEach(function (el, index) {
         el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
     })
@@ -68,7 +71,7 @@ function showSearch() {
 }
 function hideSearch() {
     headerEl.classList.remove('searching');
-    document.documentElement.classList.remove('fixed')
+    playScroll()
     headerMenuEls.reverse().forEach(function (el, index) {
         el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
     })
@@ -78,6 +81,75 @@ function hideSearch() {
     searchDelayEls.reverse()
     searchInputEl.value = '';
 }
+function playScroll() {
+    document.documentElement.classList.remove('fixed')
+}
+function stopScroll() {
+    document.documentElement.classList.add('fixed')
+}
+
+//헤더 메뉴 토글!
+const menuStarterEl = document.querySelector('header .menu-starter');
+menuStarterEl.addEventListener('click', function () {
+    if (headerEl.classList.contains('menuing')) {
+        headerEl.classList.remove('menuing')
+        searchInputEl.value = '';
+        playScroll()
+    } else {
+        headerEl.classList.add('menuing');
+        stopScroll()
+    }
+})
+
+
+//헤더 검색!!
+const searchTextfieldEl = document.querySelector('header .textfield');
+const searchCancelEl = document.querySelector('header .search-canceler');
+searchTextfieldEl .addEventListener('click', function () {
+    headerEl.classList.add('searching--mobile');
+    searchInputEl.focus();
+})
+searchCancelEl.addEventListener('click', function () {
+    headerEl.classList.remove('searching--mobile');
+})
+
+
+
+//
+window.addEventListener('resize', function () {
+    if (window.innerWidth <= 740) {
+        headerEl.classList.remove('searching');
+    }else{
+        headerEl.classList.remove('searching--mobile');
+    }
+})
+
+
+const navEl = document.querySelector('nav')
+const navMenuToggleEl = navEl.querySelector('.menu-toggler')
+const navMenuShadowEl = navEl.querySelector('.shadow')
+
+navMenuToggleEl.addEventListener('click', function () {
+    if(navEl.classList.contains('menuing')) {
+        hideNavMenu();
+    }else{
+        showNavMenu();
+    }
+})
+navEl.addEventListener('click', function (event) {
+    event.stopPropagation();
+})
+navMenuShadowEl.addEventListener('click', hideNavMenu);
+window.addEventListener('click', hideNavMenu);
+function showNavMenu() {
+    navEl.classList.add('menuing');
+}
+function hideNavMenu() {
+    navEl.classList.remove('menuing');
+}
+
+
+
 
 
 
@@ -159,6 +231,7 @@ navigations.forEach(function (nav) {
     mapEl.innerHTML = /*html*/ `
     <h3>
         <span class="text">${nav.title}</span>
+        <span class="icon">+</span>
     </h3>
     <ul>
         ${mapList}
@@ -173,3 +246,12 @@ navigations.forEach(function (nav) {
 
 const thisYearEl = document.querySelector('span.this-year');
 thisYearEl.textContent = new Date().getFullYear();
+
+
+const mapEls = document.querySelectorAll('footer .navigations .map');
+mapEls.forEach(function (el) {
+    const h3El = el.querySelector('h3');
+    h3El.addEventListener('click', function () {
+        el.classList.toggle('active');
+    })
+})
